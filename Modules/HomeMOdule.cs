1067f60ef97ed0df6be6177447f01917ad935782
Levi Bibo and Steve Burton <levi+steve@>
2016-11-30 11:18:05 -0800

    add input and result pages; add css formatting.

--- Modules/HomeMOdule.cs
+++ Modules/HomeMOdule.cs
@@ -12,6 +12,27 @@ namespace RPS
 			{
 				return View["index.cshtml"];
 			};
+			Post["Rock-Paper-Scissors/player1-choice"] = _ =>
+			{
+				string player1Name = Request.Form["player1Name"];
+				string player2Name = Request.Form["player2Name"];
+				RockPaperScissors newGame = new RockPaperScissors(player1Name, player2Name);
+				return View["player1Input.cshtml", newGame];
+			};
+			Post["Rock-Paper-Scissors/player2-choice"] = _ =>
+			{
+				int player1Choice = int.Parse(Request.Form["choice"]);
+				RockPaperScissors newGame = RockPaperScissors.GetGame();
+				newGame.SetPlayer1Choice(player1Choice);
+				return View["player2Input.cshtml", newGame];
+			};
+			Post["Rock-Paper-Scissors/result"] = _=>
+			{
+				int player2Choice = int.Parse(Request.Form["choice"]);
+				RockPaperScissors newGame = RockPaperScissors.GetGame();
+				newGame.SetPlayer2Choice(player2Choice);
+				return View["result.cshtml", newGame];
+			};
 		}
 	}
 }
--- Objects/RockPaperScissors.cs
+++ Objects/RockPaperScissors.cs
@@ -10,11 +10,18 @@ namespace RPS.Objects
 		private string _player2Name;
 		private int _player1Choice;
 		private int _player2Choice;
+		private static Dictionary<int, RockPaperScissors> _games = new Dictionary<int, RockPaperScissors>();
+
+		public static RockPaperScissors GetGame()
+		{
+			return _games[0];
+		}
 
 		public RockPaperScissors(string name1, string name2)
 		{
 			_player1Name = name1;
 			_player2Name = name2;
+			_games[0] = this;
 		}
 
 		public void SetPlayer1Choice(int choice)
@@ -25,6 +32,26 @@ namespace RPS.Objects
 		public void SetPlayer2Choice(int choice)
 		{
 			_player2Choice = choice;
+		}
+
+		public string GetPlayer1Name()
+		{
+			return _player1Name;
+		}
+
+		public string GetPlayer2Name()
+		{
+			return _player2Name;
+		}
+
+		public int GetPlayer1Choice()
+		{
+			return _player1Choice;
+		}
+
+		public int GetPlayer2Choice()
+		{
+			return _player2Choice;
 		}
 
 		public string GetWinner()
